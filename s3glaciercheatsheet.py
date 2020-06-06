4e82169ff0c4df7f3ff4bcb60cc8590cd14c374f
Tim Nibert <docz2a@gmail.com>
2020-06-06 20:54:24 +1000

    updated cheat sheet for s3, made subdirectories

--- s3glaciercheatsheet.py
+++ s3glaciercheatsheet.py
@@ -6,6 +6,11 @@ s3 = boto3.resource(‘s3’)
 for bucket in s3.buckets.all():
     print(bucket.name)
 
+# Upload a new file
+data = open('test.jpg', 'rb')
+s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
+
+
 #Python Examples of using AWS Glacier
 
 #import boto3
