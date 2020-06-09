abf59636b948abccd76d20eac2b34d2e27c5c322
Tim Nibert <docz2a@gmail.com>
2020-06-09 20:02:22 +1000

    untested adjustments to multipart upload

--- glacier/multipart.py
+++ glacier/multipart.py
@@ -1,6 +1,8 @@
+from botocore.utils import calculate_tree_hash
 import boto3
+import pprint
 
-# from http://blog.ragsagar.in/2015/10/03/large-file-upload-using-amazon-glacier-boto3.html
+# based on http://blog.ragsagar.in/2015/10/03/large-file-upload-using-amazon-glacier-boto3.html
 
 # I am uploading as chunks of 32mb
 CHUNK_SIZE = 1048576 * 32 
@@ -8,26 +10,27 @@ CHUNK_SIZE = 1048576 * 32
 #AWS_SECRET_ACCESS_KEY = "Your secret access key"
 
 
-def upload_large_file(vault_name, region_name, filepath, description):
-    #session = session(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=region_name)
-    glacier = session.resource("glacier")
+def upload_large_file(vault_name, filepath, description):
+    glacier = boto3.resource("glacier")
     vault = glacier.Vault(account_id="-", name=vault_name)
 
     multipart_upload = vault.initiate_multipart_upload(accountId="-", archiveDescription=description, partSize=str(CHUNK_SIZE))
     upload_id = multipart_upload.id
+    print("Upload id: {}".format(upload_id))
 
-    f = open(filepath)
-    start_range = 0
-    for chunk in read_in_chunks(f, CHUNK_SIZE):
-        range_data = "bytes %s-%s/*" % (start_range, f.tell()-1)
-        print("Uploading range %s" % range_data)
-        multipart_upload.upload_part(range=range_data, body=chunk)
-        start_range = f.tell()
+    with open(filepath, 'rb') as f:
+        start_range = 0
+        for chunk in read_in_chunks(f, CHUNK_SIZE):
+            range_data = "bytes %s-%s/*" % (start_range, f.tell()-1)
+            print("Uploading range %s" % range_data)
+            multipart_upload.upload_part(range=range_data, body=chunk)
+            start_range = f.tell()
 
-    f.seek(0)
-    response = multipart_upload.complete(archiveSize=str(start_range),
-                                         checksum=calculate_tree_hash(f))
-    f.close()
+        f.seek(0)
+        response = multipart_upload.complete(archiveSize=str(start_range),
+                                             checksum=calculate_tree_hash(f))
+    pprint.pprint(response)
+
     archive_id = response.get('archiveId')
     return archive_id
 
